d0c09f1a0ddc8dd43bc31d77dd95577aca4c6430
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2023-05-28 15:51:11 +0200

    Ajout de la modal [WIP]

--- public/index.php
+++ public/index.php
@@ -13,7 +13,48 @@ include_once __DIR__ . '/m/requetes.php';
     <link rel="stylesheet" href="css/style.css?v=<?=uniqid()?>">
 </head>
 <body>
-    <div id="tables-stages">
+    <div id="tables-stages"></div>
+    <div class="container">
+        <a data-modal="modal-one">Open Modal</a>
+    </div>
+
+    <div class="modal" id="modal-one">
+        <div class="modal-bg"></div> <!-- Ajouter la classe "modal-exit" si on souhaite quitter la modale en cliquant à l'extérieur -->
+        <div class="modal-container">
+            <button class="modal-close modal-exit">X</button>
+            <div>
+                <label for="horaires_mois_1">
+                    <input type="checkbox" name="horaires_mois_1" id="horaires_mois_1" checked>
+                    Horaires mois 1
+                </label>
+            </div>
+            <div>
+                <label for="horaires_mois_2">
+                    <input type="checkbox" name="horaires_mois_2" id="horaires_mois_2" checked>
+                    Horaires mois 2
+                </label>
+            </div>
+            <div>
+                <label for="horaires_mois_3">
+                    <input type="checkbox" name="horaires_mois_3" id="horaires_mois_3" checked>
+                    Horaires mois 3
+                </label>
+            </div>
+            <div>
+                <label for="attestation_de_stage">
+                    <input type="checkbox" name="attestation_de_stage" id="attestation_de_stage" checked>
+                    Attestation de stage
+                </label>
+            </div>
+            <div>
+                <label for="evaluation_de_stage">
+                    <input type="checkbox" name="evaluation_de_stage" id="evaluation_de_stage" checked>
+                    Évaluation de stage
+                </label>
+            </div>
+            <button class="modal-exit">Annuler</button>
+            <button class="modal-send modal-exit">Envoyer</button>
+        </div>
     </div>
     <script src="js/script.js?v=<?=uniqid()?>"></script>
 </body>
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,20 @@
+const modals = document.querySelectorAll('[data-modal]');
+
+modals.forEach(function (trigger) {
+  trigger.addEventListener('click', function (event) {
+    event.preventDefault();
+    const modal = document.getElementById(trigger.dataset.modal);
+    modal.classList.add('open');
+    const exits = modal.querySelectorAll('.modal-exit');
+    exits.forEach(function (exit) {
+      exit.addEventListener('click', function (event) {
+        event.preventDefault();
+        modal.classList.remove('open');
+      });
+    });
+  });
+});
+
 async function recupererDonnees() {
     var formData = new FormData();
     formData.append('recupererDonnees', 1);
@@ -6,10 +23,10 @@ async function recupererDonnees() {
         method: "POST",
         body: formData
     })
-    .then((response) => response.json())
-    .then((result) => {
-        document.getElementById("tables-stages").innerHTML = result;
-    });
+        .then((response) => response.json())
+        .then((result) => {
+            document.getElementById("tables-stages").innerHTML = result;
+        });
 
 }
 
